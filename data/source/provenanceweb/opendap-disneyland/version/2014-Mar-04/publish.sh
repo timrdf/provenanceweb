3306dc51a709d94cf8796bd5b21fc5e52929dda9
Timothy Lebo <timrdf@gmail.com>
2014-03-04 15:37:04 +0000

    disneyland example client

---
+++ data/source/provenanceweb/opendap-disneyland/version/2014-Mar-04/publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#3> <> a conversion:PublicationTrigger; # Could also be conversion:Idempotent;
+#3>    foaf:name    "publish.sh";
+#3>    rdfs:seeAlso <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Triggers#wiki-4-publication-triggers>;
+#3> .
+#
+
+cr-publish.sh source/cr-droid.ttl source/CA_OrangeCo_2011_000402.txt.cdl.nc source/CA_OrangeCo_2011_000402.txt.cdl.nc.prov.ttl \
+   automatic/CA_OrangeCo_2011_000402.png automatic/cr-droid.ttl
