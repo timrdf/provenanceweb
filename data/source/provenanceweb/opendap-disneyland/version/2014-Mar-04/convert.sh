42241569d72df5a38086a71e58260b58cafa208e
Timothy Lebo <timrdf@gmail.com>
2014-03-04 15:22:37 +0000

    disneyland demo

---
+++ data/source/provenanceweb/opendap-disneyland/version/2014-Mar-04/convert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#3> <> a conversion:ConversionTrigger; # Could also be conversion:Idempotent;
+#3>    foaf:name    "convert.sh";
+#3>    rdfs:seeAlso <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Triggers#wiki-3-computation-triggers>;
+#3> .
+#
+
+mkdir -p automatic
+
+if [ ! -e automatic/CA_OrangeCo_2011_000402.png ]; then
+   # Pretend to render the LiDAR
+   curl http://provenanceweb.org/CA_OrangeCo_2011_000402.png > automatic/CA_OrangeCo_2011_000402.png
+   cr-droid.sh automatic/CA_OrangeCo_2011_000402.png > automatic/cr-droid.ttl
+fi
---
+++ data/source/provenanceweb/opendap-disneyland/version/2014-Mar-04/retrieve.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#3> @prefix doap:    <http://usefulinc.com/ns/doap#> .
+#3> @prefix dcterms: <http://purl.org/dc/terms/> .
+#3> @prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#> .
+#3> 
+#3> <> a conversion:RetrievalTrigger, doap:Project; # Could also be conversion:Idempotent;
+#3>    dcterms:description 
+#3>      "Script to retrieve and convert a new version of the dataset.";
+#3>    rdfs:seeAlso 
+#3>      <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Automated-creation-of-a-new-Versioned-Dataset>,
+#3>      <https://github.com/timrdf/csv2rdf4lod-automation/wiki/tic-turtle-in-comments>;
+#3> .
+
+mkdir -p source automatic
+
+echo source/CA_OrangeCo_2011_000402.txt.cdl.nc
+pushd source
+   if [ ! -e CA_OrangeCo_2011_000402.txt.cdl.nc ]; then
+      pcurl.sh 'http://opendap.tw.rpi.edu/tomcat/opendap/ipaw2014/disneyland/CA_OrangeCo_2011_000402.txt.cdl.nc'
+   fi
+   cr-droid.sh source/CA_OrangeCo_2011_000402.txt.cdl.nc > source/cr-droid.ttl
+popd
