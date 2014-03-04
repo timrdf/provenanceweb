9ea08529d2220fbc1ab25a95b87c2e2b20b89db8
Timothy Lebo <timrdf@gmail.com>
2014-03-04 21:13:51 +0000

    new request

--- data/source/provenanceweb/opendap-disneyland/version/2014-Mar-04/pingback.sh
+++ data/source/provenanceweb/opendap-disneyland/version/2014-Mar-04/pingback.sh
@@ -2,7 +2,7 @@
 #3> <> rdfs:seeAlso <https://github.com/tetherless-world/opendap/wiki/Use-case:-Disneyland#wiki-the-clients-derivation-rendering-the-lidar-as-an-image> .
 
 # /home/prizms/prizms/opendap/data/source/us/opendap-prov/version
-pingbackURI='http://opendap.tw.rpi.edu/prov-pingback/20140304-1393964393-ab12/mykey'
+pingbackURI='http://opendap.tw.rpi.edu/prov-pingback/20140304-1393967542-ab12/mykey'
 
 prov='http://provenanceweb.org/source/provenanceweb/file/opendap-disneyland/version/2014-Mar-04/conversion/provenanceweb-opendap-disneyland-2014-Mar-04.ttl.gz'
 
