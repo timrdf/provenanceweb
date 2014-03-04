2b5bb627d2f44e76faa93ab9b760115e1132db39
Timothy Lebo <timrdf@gmail.com>
2014-03-04 20:18:58 +0000

    cr-droid relative path

--- data/source/provenanceweb/opendap-disneyland/version/2014-Mar-04/convert.sh
+++ data/source/provenanceweb/opendap-disneyland/version/2014-Mar-04/convert.sh
@@ -11,7 +11,8 @@ mkdir -p automatic
 if [ ! -e automatic/CA_OrangeCo_2011_000402.png ]; then
    # Pretend to render the LiDAR
    curl http://provenanceweb.org/CA_OrangeCo_2011_000402.png > automatic/CA_OrangeCo_2011_000402.png
-   cr-droid.sh automatic/CA_OrangeCo_2011_000402.png > automatic/cr-droid.ttl
+   pushd automatic
+      cr-droid.sh CA_OrangeCo_2011_000402.png >> cr-droid.ttl
+   popd
    justify.sh source/CA_OrangeCo_2011_000402.txt.cdl.nc automatic/CA_OrangeCo_2011_000402.png render
-
 fi
