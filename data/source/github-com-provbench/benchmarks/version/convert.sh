c00395f543245f4e848964eb6d8b1d0ffa2133c8
Tim L <timrdf@gmail.com>
2014-06-10 20:36:48 +0200

    doap modeling

--- data/source/github-com-provbench/benchmarks/version/convert.sh
+++ data/source/github-com-provbench/benchmarks/version/convert.sh
@@ -6,15 +6,15 @@
 #3> .
 #
 
-echo "@prefix doap: <http://usefulinc.com/ns/doap#>."   > automatic/repos.csv.ttl
-echo "@prefix dcterms: <http://purl.org/dc/terms/>."   >> automatic/repos.csv.ttl
-echo
+echo "@prefix doap:    <http://usefulinc.com/ns/doap#>." > automatic/repos.csv.ttl
+echo "@prefix dcterms: <http://purl.org/dc/terms/>."    >> automatic/repos.csv.ttl
+echo                                                    >> automatic/repos.csv.ttl
 
 for repo in `cat automatic/repos.csv`; do
         hash=`md5.sh -qs $repos`
-        echo "<$hash> a doap:GitRepository;"           >> automatic/repos.csv.ttl
-        echo "        doap:location <$repo>;"          >> automatic/repos.csv.ttl
-        echo "."                                       >> automatic/repos.csv.ttl
-        echo "<$hash> dcterms:isReferencedBy <$repo>." >> automatic/repos.csv.ttl
+        echo "<$hash> a doap:GitRepository;"            >> automatic/repos.csv.ttl
+        echo "        doap:location <$repo>;"           >> automatic/repos.csv.ttl
+        echo "."                                        >> automatic/repos.csv.ttl
+        echo "<$hash> dcterms:isReferencedBy <$repo>."  >> automatic/repos.csv.ttl
 done
 justify.sh automatic/repos.csv automatic/repos.csv.ttl TODO
